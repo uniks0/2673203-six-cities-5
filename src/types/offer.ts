fbd0718a0377595ee1d8ad6f6d1a07b303bbf07e
Плюснина Ксения <plusnina@MacBook-Pro-Plusnina-2.local>
2025-09-29 11:23:06 +0500

    добавила моки и типы для сущностей

---
+++ src/types/offer.ts
@@ -0,0 +1,49 @@
+import { User } from "./user";
+
+export type City =
+  | "Paris"
+  | "Cologne"
+  | "Brussels"
+  | "Amsterdam"
+  | "Hamburg"
+  | "Dusseldorf";
+
+export type HousingType = "apartment" | "house" | "room" | "hotel";
+
+export type Amenity = 
+  | 'Breakfast' 
+  | 'Air conditioning' 
+  | 'Laptop friendly workspace' 
+  | 'Baby seat' 
+  | 'Washer' 
+  | 'Towels' 
+  | 'Fridge';
+
+export type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
+
+export interface RentalOffer {
+  title: string;
+  description: string;
+  publicationDate: Date;
+  city: City;
+  
+  previewImage: string;
+  images: string[];
+  
+  isPremium: boolean;
+  isFavorite: boolean;
+  rating: number;
+  
+  type: HousingType;
+  bedrooms: number;
+  maxAdults: number;
+  price: number;
+  
+  amenities: Amenity[];
+  author: User;
+  commentsCount?: number;
+  location: Coordinates;
+}
---
+++ src/types/сomment.ts
@@ -0,0 +1,8 @@
+import { User } from "./user";
+
+export type Comment = {
+  text: string;
+  publicationDate: Date;
+  rating: number;
+  author: User;
+};
